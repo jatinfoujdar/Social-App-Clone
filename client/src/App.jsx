0974f7c83b3a982b6a4dc877daecbfd21ee8129b
Jatin Foujdar <jatin321001@gmail.com>
2023-09-22 22:10:48 +0530

    route commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
-import {Button} from "@chakra-ui/button"
+import {Route, Routes} from "react-router-dom"
 import { Container } from '@chakra-ui/react'
+import UserPage from "./Pages/UserPage";
+import PostPage from "./Pages/PostPage";
 
 function App(){
   return(
  
  <Container maxW='550px'>
- <Button>hello</Button>  
+<Routes>
+ <Route path= "/:username" element={<UserPage/>} />
+ <Route path= "/:username/post/:pid" element={<PostPage/>} />
+</Routes>
  </Container>
  
   )
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,37 +1,40 @@
 import React from 'react'
-import { ChakraProvider,ColorModeScript, extendTheme  } from "@chakra-ui/react"
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react"
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {mode} from "@chakra-ui/theme-tools"
-
+import { mode } from "@chakra-ui/theme-tools"
+import { BrowserRouter } from "react-router-dom"
 
 const style = {
-  global:(props)=>({
-    body:{
-      color:mode("gray.800","white.900")(props),
-      bg:mode("gray.100", "101010")(props),
+  global: (props) => ({
+    body: {
+      color: mode("gray.800", "white.900")(props),
+      bg: mode("gray.100", "101010")(props),
     }
   })
 };
 
 const config = {
-  intialColorMode: "dark",
-  usesystemColorMode: true,
+  initialColorMode: "dark", 
+  useSystemColorMode: true, 
 };
+
 const color = {
-  gray:{
+  gray: {
     light: "#616161",
     dark: "#1e1e1e"
   }
 }
 
-const theme = extendTheme({config, style,color})
+const theme = extendTheme({ config, styles: style, colors: color }) 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
