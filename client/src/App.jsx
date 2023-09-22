69ddcf08c2cbcaaa92aa8a1918e6e0bc0b82255f
Jatin Foujdar <jatin321001@gmail.com>
2023-09-22 22:27:57 +0530

    bug fixed commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import {Route, Routes} from "react-router-dom"
 import { Container } from '@chakra-ui/react'
 import UserPage from "./Pages/UserPage";
 import PostPage from "./Pages/PostPage";
+import Header from "./component/Header";
 
 function App(){
   return(
  
  <Container maxW='550px'>
+  <Header/>
 <Routes>
  <Route path= "/:username" element={<UserPage/>} />
  <Route path= "/:username/post/:pid" element={<PostPage/>} />
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,28 +5,28 @@ import App from './App.jsx'
 import { mode } from "@chakra-ui/theme-tools"
 import { BrowserRouter } from "react-router-dom"
 
-const style = {
-  global: (props) => ({
-    body: {
-      color: mode("gray.800", "white.900")(props),
-      bg: mode("gray.100", "101010")(props),
-    }
-  })
+const styles = {
+	global: (props) => ({
+		body: {
+			color: mode("gray.800", "whiteAlpha.900")(props),
+			bg: mode("gray.100", "#101010")(props),
+		},
+	}),
 };
 
 const config = {
-  initialColorMode: "dark", 
-  useSystemColorMode: true, 
+	initialColorMode: "dark",
+	useSystemColorMode: true,
 };
 
-const color = {
-  gray: {
-    light: "#616161",
-    dark: "#1e1e1e"
-  }
-}
+const colors = {
+	gray: {
+		light: "#616161",
+		dark: "#1e1e1e",
+	},
+};
 
-const theme = extendTheme({ config, styles: style, colors: color }) 
+const theme = extendTheme({ config, styles, colors });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
