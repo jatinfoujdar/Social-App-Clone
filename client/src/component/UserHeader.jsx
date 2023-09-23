d3b0dc3571ed1beff6e068981660d538f5159e9a
Jatin Foujdar <jatin321001@gmail.com>
2023-09-24 01:08:40 +0530

    icon commit

--- client/src/component/UserHeader.jsx
+++ client/src/component/UserHeader.jsx
@@ -1,20 +1,30 @@
 import { Avatar, Box, Flex, VStack,Text } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const UserHeader = () => {
   return (
     <VStack gap={4} alignItems={"start"}>
         <Flex justifyContent={"space-between"} w={"full"}>
             <Box>
-                <Text fontSize={"2xl"}>Jatin Foujdar</Text>
+                <Text fontSize={"2xl"} fontWeight={"bold"}>Jatin Foujdar</Text>
                 <Flex gap={2} alignItems={"center"}>
                 <Text fontSize={"sm"}>jatinfoujdar</Text>
                 <Text fontSize={"sm"} bg={"gray.dark"} color={"gray.light"} p={1} borderRadius={"full"} >threads.next</Text>
             </Flex>
             </Box>
             <Box>
-                <Avatar name='Jatin Foujdar' scr= "./zuck-avatar.png" size={"xl"} />
+                <Avatar name='Jatin Foujdar' scr= "/zuck-avatar.png" size={"xl"} />
             </Box>
+        </Flex>
+        <Text>I'm a learner and always Ravenous for new Skills !</Text>
+        <Flex w={"full"} justifyContent={"space-between"}>
+            <Flex gap={2} alignItems={"center"}>
+                <Text color={"gray.light"}>3.2k followers</Text>
+                <Box w={"1"} h={"1"} bg={"gray.light"} borderRadius={"full"}></Box>
+                <Link color={'gray.light'}>instagram.com</Link>
+            </Flex>
+            <Flex gap={2} alignItems={"center"}></Flex>
         </Flex>
     </VStack>
   )
