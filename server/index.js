a33f068da67e683ed793f1428988f43610a74498
Jatin Foujdar <jatin321001@gmail.com>
2023-10-03 00:37:57 +0530

    mongodb setup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./database/db.js";
 
+
+dotenv.config();
+connectDB();
 const app = express();
 
-app.listen(3000,console.log("server started at http://localhost:3000/"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>console.log(`Server started at http://localhost:${PORT}`));
