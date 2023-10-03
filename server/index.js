5609b471835ddfe1090478563a700ad21f90aaf6
Jatin Foujdar <jatin321001@gmail.com>
2023-10-03 23:10:44 +0530

    sign-up routes commit

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./database/db.js";
 import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js"
 
 
 dotenv.config();
@@ -11,8 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}))
 app.use(cookieParser());
+
+app.use("/api/users",userRoutes)
 
 
 app.listen(PORT,()=>console.log(`Server started at http://localhost:${PORT}`));
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/signup",(req,res)=>{
+    res.send("Signed-up successfully");
+})
+
+
+export default router;
