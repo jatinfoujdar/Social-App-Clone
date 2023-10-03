45d7664d0471436d7aee79aee0893f6f9446a589
Jatin Foujdar <jatin321001@gmail.com>
2023-10-03 21:28:46 +0530

    middleware commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./database/db.js";
+import cookieParser from "cookie-parser";
 
 
 dotenv.config();
@@ -8,5 +9,10 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
 
 app.listen(PORT,()=>console.log(`Server started at http://localhost:${PORT}`));
