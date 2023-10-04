7778b4ff72bd6c45de7a6f763d0c6c8cef6ae954
Jatin Foujdar <jatin321001@gmail.com>
2023-10-05 01:37:19 +0530

    login route commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,7 @@ import User from "../schema/userSchema.js";
 import bcrypt from "bcryptjs";
 import generateJWT from "../utils/generateJWT.js";
 
-const signupUser = async(req,res)=>{
+export const signupUser = async(req,res)=>{
   
     try {
         const {name,email,username,password} = req.body;
@@ -44,7 +44,28 @@ const signupUser = async(req,res)=>{
 }
 
 
+export const loginUser =  async(req,res)=>{
+    try {
+        const {username,password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcrypt.compare(password,user?.password || "");
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({message: "Invaild Username and Password"});
+        }
+        generateJWT(user._id,res);
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            username: user.username,
+        })
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        console.log("Error is login User", error.message);
+    }
+}
 
 
 
-export {signupUser} ;
