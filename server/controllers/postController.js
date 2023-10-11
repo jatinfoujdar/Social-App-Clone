659d41d16a8dd88921e62b189653f22ec18ad2df
Jatin Foujdar <jatin321001@gmail.com>
2023-10-12 02:24:37 +0530

    like/unlike post commit

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -70,3 +70,29 @@ export const deletePost = async(req,res)=>{
 		console.log("Error in deletePost: ", error.message);
     }
 }
+
+export const likeUnlikePost = async(req,res)=>{
+try {
+    const {id:postId} = req.params;
+    const userId = req.user._id;
+
+    const post = await Post.findById(postId);
+    if(!post){
+        return res.status(404).json({message: "Post not found"})
+    }
+    const userLikedPost = post.likes.includes(userId);
+    if(userLikedPost){
+     await Post.updateOne({_id:postId},{$pull: {likes: userId}})
+     res.status(200).json({message: "Post Unlikes successfully"})
+    }else{
+        post.likes.push(userId);
+        await post.save();
+        res.status(200).json({message: "Post Liked successfully"})
+    }
+
+
+} catch (error) {
+    res.status(500).json({ error: error.message });
+		console.log("Error in likeUnlikePost: ", error.message);
+}
+}
