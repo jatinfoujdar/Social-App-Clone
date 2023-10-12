8e328b7eacbc87c389b5f5c3d06346b3ab692755
Jatin Foujdar <jatin321001@gmail.com>
2023-10-12 17:56:05 +0530

    reply commit

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -99,27 +99,27 @@ try {
 
 export const replyPost = async(req,res)=>{
     try {
-        const {text} = req.body;
-        const postId = req.params._id;
-        const userId = req.user._id;
-        const userProfilePic = req.user.profilePic;
-        const username = req.user.username;
+		const { text } = req.body;
+		const postId = req.params.id;
+		const userId = req.user._id;
+		const userProfilePic = req.user.profilePic;
+		const username = req.user.username;
 
-        if(!text){
-            return res.status(400).json({message: "Text field is required"})
-        }
-        const post = await Post.findById(postId);
-        if(!post){
-            return res.status(404).json({message: "Post not found"})
-        }
-        const reply = {userId,text,userProfilePic,username};
+		if (!text) {
+			return res.status(400).json({ error: "Text field is required" });
+		}
 
-        post.replies.puch(reply);
-        await post.save();
-        
+		const post = await Post.findById(postId);
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
 
-        res.status(200).json({message: "Reply added successfully",post})
-        
+		const reply = { userId, text, userProfilePic, username };
+
+		post.replies.push(reply);
+		await post.save();
+
+		res.status(200).json(reply);        
     } catch (error) {
         res.status(500).json({ error: error.message });
 		console.log("Error in replyPost: ", error.message);
