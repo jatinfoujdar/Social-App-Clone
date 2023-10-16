bd22d69e8f3014833be46e4da01a8f9674720a6a
Jatin Foujdar <jatin321001@gmail.com>
2023-10-17 00:11:54 +0530

    login/signup  commmit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { Container } from '@chakra-ui/react'
 import UserPage from "./Pages/UserPage";
 import PostPage from "./Pages/PostPage";
 import Header from "./component/Header";
+import HomePage from "./Pages/HomePage";
+import AuthPage from "./Pages/AuthPage";
 
 function App(){
   return(
@@ -10,6 +12,8 @@ function App(){
  <Container maxW='550px'>
   <Header/>
 <Routes>
+  <Route path="/" element={<HomePage/>}/>
+  <Route path="/auth" element={<AuthPage/>}/>
  <Route path= "/:username" element={<UserPage/>} />
  <Route path= "/:username/post/:pid" element={<PostPage/>} />
 </Routes>
---
+++ client/src/Pages/AuthPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Signup from '../component/Signup'
+
+
+const AuthPage = () => {
+  return ( 
+  <Signup/>
+  )
+}
+
+export default AuthPage
