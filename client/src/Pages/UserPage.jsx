b643a4a4056c155df9a7b942be444837ea7290e6
Jatin Foujdar <jatin321001@gmail.com>
2023-10-28 22:34:29 +0530

    solve infinite problem commit

--- client/src/Pages/UserPage.jsx
+++ client/src/Pages/UserPage.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UserHeader from '../component/UserHeader'
 import UserPost from '../component/userPost'
+import { useParams } from 'react-router-dom';
+import useShowToast from '../hooks/useShowToast';
 
 const UserPage = () => {
+  const [user,setUser] = useState(null);
+  const {username} = useParams();
+  const showToast = useShowToast();
+
+  useEffect(()=>{
+    const getUser = async()=>{
+      try {
+        const res = await fetch(`/api/users/profile/${username}`)
+        const data = await res.json()
+        // console.log(data);
+        if(data.error){
+          showToast("Error",data.error,"error");
+          return;
+        }
+        setUser(data)
+      } catch (error) {
+        console.log("Error",error,"error");
+      }
+    }
+    getUser();
+  },[username,showToast]);
+
   return (
     <>
-    <UserHeader/>
+    <UserHeader user={user}/>
     <UserPost likes={1100} replies={112} postImg="/jf.jpg" postTitle="let's work"/>
     <UserPost likes={1200} replies={112} postImg="/post1.png" postTitle="let's work"/>
     <UserPost likes={100} replies={12} postImg="/post2.png" postTitle="let's work"/>
--- client/src/hooks/useShowToast.js
+++ client/src/hooks/useShowToast.js
@@ -4,8 +4,7 @@ import { useCallback } from "react";
 const useShowToast = () => {
   const toast = useToast();
 
-  const showToast = useCallback(
-    (title, description, status) => {
+  const showToast = useCallback((title, description, status) => {
       toast({
         title,
         description,
