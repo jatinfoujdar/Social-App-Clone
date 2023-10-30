a0aad82ef957ff1aef612f41d4dc28bf90177bcd
Jatin Foujdar <jatin321001@gmail.com>
2023-10-30 21:35:42 +0530

    modal commit

--- client/src/component/CreatePost.jsx
+++ client/src/component/CreatePost.jsx
@@ -1,11 +1,31 @@
 import { AddIcon } from '@chakra-ui/icons'
 import { Button, useColorModeValue } from '@chakra-ui/react'
+
 import React from 'react'
 
 const CreatePost = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <>
     <Button position={'fixed'} bottom={10} right={10} leftIcon={<AddIcon/>} bg={useColorModeValue("gray.300","gray.dark")}></Button>
+   
+    <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Lorem count={2} />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   )
 }
